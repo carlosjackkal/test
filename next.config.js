3a33230060343944525f355d81e4f41b1611929c
carlosjackkal <carlosjackkal@proton.me>
2026-02-13 20:02:59 -0500

    added test subdirectory for ghpages deploy

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  basePath: '/test', // Replace 'your-base-path' with the desired base path for your deployment
+//   assetPrefix: '/your-base-path/', // Ensures assets are served from the correct base path
+};
