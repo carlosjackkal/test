186381aad4d039c4b7eb03c7a60554053bd55496
carlosjackkal <carlosjackkal@proton.me>
2026-02-15 10:14:38 -0500

    changed to build output 'export'

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   basePath: '/test', // Ensures the app is served from the /test subdirectory
   assetPrefix: '/test', // Ensures assets are served from the /test subdirectory
   trailingSlash: true, // Ensures paths end with a trailing slash for GitHub Pages compatibility
+  output: 'export',       // Enables static export
+  images: {
+    unoptimized: true,    // Required for static export (no Image Optimization)
+  },
 };
