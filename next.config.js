15004b882db3f493369d05aa5ba16dd51bce6510
carlosjackkal <carlosjackkal@proton.me>
2026-02-25 13:44:30 -0500

    chore: add build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  basePath: "", // Ensures the app is served from the /test subdirectory
-  assetPrefix: "", // Ensures assets are served from the /test subdirectory
+  basePath: "/test", // Ensures the app is served from the /test subdirectory
+  assetPrefix: "/test", // Ensures assets are served from the /test subdirectory
   trailingSlash: true, // Ensures paths end with a trailing slash for GitHub Pages compatibility
   output: "export", // Enables static export
   images: {
