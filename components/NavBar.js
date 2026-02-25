1a97f338a13c34e287d28678652a426669f31267
carlosjackkal <carlosjackkal@proton.me>
2026-02-25 13:22:16 -0500

    feat: added responsive navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,38 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
-import { Workflow } from "../pages/_app";
 
-export default function NavBar({
-  selectedWorkflow,
-  setSelectedWorkflow,
-  navLinks,
-}) {
+export default function NavBar({ navLinks }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleResize = () => setIsMobile(mediaQuery.matches);
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   return (
     <nav className="nav">
+      {isMobile && 
+      <div className="nav-container-mobile">
+        <div className="nav-logo">
+          <Link href="/" style={{ marginRight: 12 }}>
+            BTCDecoded
+          </Link>
+        </div>
+        <div className="nav-links">
+          {navLinks.map((link, index) => (
+            <Link key={index} href={link.href} style={{ marginRight: 12 }}>
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      </div>
+      }
+      {!isMobile &&
       <div className="nav-container">
         <div className="nav-logo">
           <Link href="/" style={{ marginRight: 12 }}>
@@ -22,6 +47,7 @@ export default function NavBar({
           ))}
         </div>
       </div>
+      }
     </nav>
   );
 }
